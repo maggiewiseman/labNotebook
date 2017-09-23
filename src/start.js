4499660468b4ecf557f29b02868ec2a0b5c0484a
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-23 16:58:07 +0200

    set up teacher routes with redux

--- src/start.js
+++ src/start.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
-import { createStore, applyMiddleware } from 'redux';
-//import reducer from './reducers';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import { Provider } from 'react-redux';
 
+//Reducers
+import teacherReducer from './teacher/reducer';
+import studentReducer from './student/reducer';
 
-//Components
+//Component Import
 import Welcome from './auth/welcome';
 import Registration from './auth/registration';
 import StudentApp from './student/app';
 import StudentDashboard from './student/dashboard';
 import TeacherApp from './teacher/views/app';
 import TeacherDashboard from './teacher/views/dashboard';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-// const store = createStore(reducers, composeWithDevTools(applyMiddleware(reduxPromise)));
+//Redux Setup
+//when we edit the state object in teacherReduer only the teacher part of state will change. It will be unable to overwrite anything in studetns.
+const reducers = combineReducers({
+    teachers: teacherReducer,
+    students: studentReducer,
+});
 
+ const store = createStore(reducers, composeWithDevTools(applyMiddleware(reduxPromise)));
+
+//Routers
 const loggedOutRouter = (
     <Router history={browserHistory}>
         <Route path="/" component={Welcome}>
@@ -32,24 +45,23 @@ const studentRouter = (
     </Router>
 )
 
-// const teacherRouter = (
-//     // <Provider store={store}>
-//     //     <Router history={browserHistory}>
-//     //         <Route path="/teacher" component={TeacherApp}>
-//     //             <IndexRoute component={TeacherDashboard}/>
-//     //         </Route>
-//     //     </Router>
-//     // </Provider>
-// )
+const teacherRouter = (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/teacher" component={TeacherApp}>
+                <IndexRoute component={TeacherDashboard}/>
+            </Route>
+        </Router>
+    </Provider>
+)
 
 let route = loggedOutRouter;
 if (location.pathname == '/student') {
     console.log('using student router');
     route = studentRouter;
+} else if (location.pathname == '/teacher') {
+    route = teacherRouter;
 }
-// else if (location.pathname == '/teacher') {
-//     route = teacherRouter;
-// }
 
 ReactDOM.render(
     route,
--- src/teacher/views/app.js
+++ src/teacher/views/app.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { connect, Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { connect } from 'react-redux';
 
 class App extends React.Component  {
     render() {
         return (
             <div>
                 Hello World from Teacher App
-                {props.children}
             </div>
         );
     };
