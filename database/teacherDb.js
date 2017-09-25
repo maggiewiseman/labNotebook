e240a2781eadf95399d37fa31a41d4d7965a1c24
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-25 15:19:14 +0200

    wrote new query for inserting assignment into db

--- database/teacherDb.js
+++ database/teacherDb.js
@@ -9,12 +9,30 @@ var dbUrl = process.env.DATABASE_URL || localUrl;
 
 var db = spicedPg(dbUrl);
 /********** ASSIGNMENTS *********/
+function saveNewAssignmentTemplate(data) {
+    console.log('DBQUERY: saveNewAssignmentTemplate,', data);
+    let queryStr = 'INSERT INTO assignments (section_id, group_lab, name, instructions, title, abstract, question, hypothesis, variables, materials, procedures, data, calculations, discussion) VALUES ($1, $2, $3, #4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)';
+    return db.query(queryStr, data);
+}
+
+module.exports.saveNewAssignmentTemplate = saveNewAssignmentTemplate;
+
 /********** STUDENTS ************/
-function getStudentsBySectionId(data){
-    console.log('DBQUERY: getStudentsBySection,', data);
+function getStudentIdsBySectionId(data){
+    console.log('DBQUERY: getStudentIdBySection,', data);
+    let queryStr = 'SELECT user_id FROM users_sections WHERE section_id = $1';
+    return db.query(queryStr, data);
+}
+
+function getStudentDataBySectionId(data){
+    console.log('DBQUERY: getStudentDataBySection,', data);
     let queryStr = 'SELECT users_sections.user_id, users.first_name, users.last_name, users.profile_pic FROM users_sections JOIN users ON users_sections.user_id = users.id WHERE section_id = $1';
     return db.query(queryStr, data);
 }
+
+module.exports.getStudentDataBySectionId = getStudentDataBySectionId;
+module.exports.getStudentIdsBySectionId = getStudentIdsBySectionId;
+
 /********** SECTIONS ************/
 function saveNewSection(data) {
     console.log('DBQUERY: saveNewSection,', data);
@@ -32,6 +50,12 @@ function getSectionsByCourseId(data) {
     let queryStr = 'SELECT sections.id, sections.name, sections.start_date, sections.end_date, courses.id FROM sections JOIN courses ON courses.id = sections.course_id WHERE courses.id = $1';
     return db.query(queryStr, data);
 }
+
+
+module.exports.saveNewSection = saveNewSection;
+module.exports.getSectionsByCourseId = getSectionsByCourseId;
+module.exports.getAllSections = getAllSections;
+
 /********* COURSES *************/
 function saveNewCourse(data) {
     console.log('DBQUERY: saveNewCourse,', data);
@@ -52,15 +76,10 @@ function deleteCourse(id) {
 }
 
 
-module.exports.getStudentsBySectionId = getStudentsBySectionId;
-
 module.exports.saveNewCourse = saveNewCourse;
 module.exports.getCoursesByTeacher = getCoursesByTeacher;
 module.exports.deleteCourse = deleteCourse;
 
-module.exports.saveNewSection = saveNewSection;
-module.exports.getSectionsByCourseId = getSectionsByCourseId;
-module.exports.getAllSections = getAllSections;
 
 
 //saveNewCourse([1, 'Biology']);
