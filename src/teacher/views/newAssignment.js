c99cef16117ec690caba56f2899732a4c93c3ad2
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-25 20:06:56 +0200

    redid event handling for input so it is easier to process

--- src/teacher/views/newAssignment.js
+++ src/teacher/views/newAssignment.js
@@ -8,62 +8,48 @@ class TeacherNewAssignment extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            AbstractEditable: true,
-            AbstractInput : "",
-            AbstractShare : false,
-            CalculationsEditable: true,
-            CalculationsInput : "",
-            CalculationsShare : false,
-            DataEditable: true,
-            DataInput : "",
-            DataShare : false,
-            DiscussionEditable: true,
-            DiscussionInput : "",
-            DiscussionShare : false,
-            HypothesisEditable: true,
-            HypothesisInput : "",
-            HypothesisShare : false,
-            MaterialsEditable: true,
-            MaterialsInput : "",
-            MaterialsShare : false,
-            ProceduresEditable: true,
-            ProceduresInput : "",
-            ProceduresShare : false,
-            QuestionInput : "",
-            QuestionShare : false,
-            TitleInput : "",
-            TitleShare : false,
-            VariablesEditable: true,
-            VariablesInput : "",
-            VariablesShare : false,
-            assignmentName : "",
-            dueDate :"",
-            groupLabCb: false,
-            includeAbstract : false,
-            includeCalculations : false,
-            includeData : false,
-            includeDiscussion : false,
-            includeHypothesis : false,
-            includeMaterials : false,
-            includeProcedures : false,
-            includeQuestion : false,
-            includeTitle : false,
-            includeVariables : false,
-            instructions : "",
-            QuestionEditable: true,
-            sectioncb3 : false,
-            TitleEditable: true
+            sections: [],
+            include: {},
+            editable: {},
+            shared: {},
+            defaults: {}
         };
         this.handleInput = this.handleInput.bind(this);
         this.submit = this.submit.bind(this);
+        this.handleSectionInput = this.handleSectionInput.bind(this);
+        this.handleIncludeInput = this.handleIncludeInput.bind(this);
+        this.handleDefaults = this.handleDefaults.bind(this);
+        this.handleEditable = this.handleEditable.bind(this);
+        this.handleShared = this.handleShared.bind(this);
     }
     componentDidMount() {
 
         this.props.dispatch(getAllSections());
 
     }
-    handleInput(event) {
+    handleSectionInput(event){
+        var name = event.target.name;
+        var sections = [...this.state.sections, name.substring(9,name.length)];
+        this.setState({
+            sections
+        }, () => {
+            console.log(this.state);
+        });
+    }
+    handleIncludeInput(event){
         const target = event.target;
+        const value = target.checked;
+        const name = target.name.substring(0, name.substring(7,name.length));
+
+        var include = Object.assign({}, this.state.include, {
+            [name]: value
+        });
+        this.setState({include}, () => {
+            console.log(this.state);
+        });
+    }
+    handleInput(event) {
+
         if(target.type == 'checkbox') {
             console.log(target.checked)
         }
@@ -76,6 +62,41 @@ class TeacherNewAssignment extends React.Component {
             console.log('New Assignment: handleInput state:', this.state);
         });
     }
+    handleDefaults() {
+        const value = event.target.value;
+        const name = event.target.name.substring(0, event.target.name.length-5);
+
+        var defaults = Object.assign({}, this.state.defaults, {
+            [name]: value
+        });
+        this.setState({defaults}, () => {
+            console.log(this.state);
+        });
+    }
+    handleEditable() {
+        const target = event.target;
+        const value = target.checked;
+        const name = target.name.substring(0, target.name.length-8);
+
+        var editable = Object.assign({}, this.state.editable, {
+            [name]: value
+        });
+        this.setState({editable}, () => {
+            console.log(this.state);
+        });
+    }
+    handleShared() {
+        const target = event.target;
+        const value = target.checked;
+        const name = target.name.substring(0, target.name.length-5);
+
+        var shared = Object.assign({}, this.state.shared, {
+            [name]: value
+        });
+        this.setState({shared}, () => {
+            console.log(this.state);
+        });
+    }
     submit() {
         this.props.dispatch(saveNewAssignment(this.state));
         //validation!
@@ -84,6 +105,13 @@ class TeacherNewAssignment extends React.Component {
     }
     render() {
         const { sections } = this.props;
+
+        var events = {
+            include: this.handleIncludeInput,
+            defaults: this.handleDefaults,
+            editable: this.handleEditable,
+            shared: this.handleShared
+        }
 
         var assignmentOptions =
                 <div >
@@ -94,16 +122,16 @@ class TeacherNewAssignment extends React.Component {
                         <p>Students can edit?</p>
                         <p>Shared amongst groups?</p>
                     </div>
-                    {createAssignmentCategoryDiv('Title', this.handleInput) }
-                    {createAssignmentCategoryDiv('Question', this.handleInput)}
-                    {createAssignmentCategoryDiv('Abstract', this.handleInput)}
-                    {createAssignmentCategoryDiv('Hypothesis', this.handleInput)}
-                    {createAssignmentCategoryDiv('Variables', this.handleInput)}
-                    {createAssignmentCategoryDiv('Materials', this.handleInput)}
-                    {createAssignmentCategoryDiv('Procedures', this.handleInput)}
-                    {createAssignmentCategoryDiv('Data', this.handleInput)}
-                    {createAssignmentCategoryDiv('Calculations', this.handleInput)}
-                    {createAssignmentCategoryDiv('Discussion', this.handleInput)}
+                    {createAssignmentCategoryDiv('Title', events) }
+                    {createAssignmentCategoryDiv('Question', events)}
+                    {createAssignmentCategoryDiv('Abstract', events)}
+                    {createAssignmentCategoryDiv('Hypothesis', events)}
+                    {createAssignmentCategoryDiv('Variables', events)}
+                    {createAssignmentCategoryDiv('Materials', events)}
+                    {createAssignmentCategoryDiv('Procedures', events)}
+                    {createAssignmentCategoryDiv('Data', events)}
+                    {createAssignmentCategoryDiv('Calculations', events)}
+                    {createAssignmentCategoryDiv('Discussion', events)}
                 </div>;
         if(!sections) {
             return null;
@@ -111,7 +139,7 @@ class TeacherNewAssignment extends React.Component {
             return (
                 <div>
                     <div>Sections list</div>
-                    {makeSectionList(sections, this.handleInput)}
+                    {makeSectionList(sections, this.handleSectionInput)}
                     <label forHtml="assignmentName">Assignment Name</label>
                     <input type="text" name="assignmentName" onChange={this.handleInput} />
                     <label forHtml="dueDate">Due Date (optional)</label>
@@ -119,7 +147,7 @@ class TeacherNewAssignment extends React.Component {
                     <label forHtml="instructions">Instructions (optional)</label>
                     <input type="textarea" rows="4" name="instructions" onChange={this.handleInput} />
                     <label forHtml="groupLabCb">Group Lab?</label>
-                    <input type="checkbox" name="groupLabCb" onChange={this.handleInput} checked />
+                    <input type="checkbox" name="groupLabCb" onChange={this.handleInput}/>
 
                     <h3>Assignment Details</h3>
                     {assignmentOptions}
@@ -140,15 +168,14 @@ const mapStateToProps = function(state) {
 }
 export default connect(mapStateToProps)(TeacherNewAssignment);
 
-
-function createAssignmentCategoryDiv(category, save) {
+function createAssignmentCategoryDiv(category, events) {
     return (
         <div style={assignmentGridStyle}>
-            <input type="checkbox" name={`include${category}`} onChange={save}/>
+            <input type="checkbox" name={`include${category}`} onChange={events.include}/>
             <label forHtml={`for${category}`}>{`${category}`}</label>
-            <input type="text" name={`${category}Input`} placeholder="Type default text here that will appear on all student assignments"  onChange={save} style={inputStyle}/>
-            <input type="checkbox" name={`${category}Editable`} checked onChange={save}/>
-            <input type="checkbox" name={`${category}Share`} onChange={save} />
+            <input type="text" name={`${category}Input`} placeholder="Type default text here that will appear on all student assignments"  onChange={events.defaults} style={inputStyle}/>
+            <input type="checkbox" name={`${category}Editable`} onChange={events.editable}/>
+            <input type="checkbox" name={`${category}Share`} onChange={events.shared} />
         </div>
     )
 }
