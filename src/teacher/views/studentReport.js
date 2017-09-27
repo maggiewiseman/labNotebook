90eba9b8f2e25c22d55f32ea539f8411dd8e0df5
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-27 15:41:58 +0200

    made basic grading assignment student report

---
+++ src/teacher/views/studentReport.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import {  } from '../actions';
+import { Collapsible, CollapsibleItem, Collection, CollectionItem} from 'react-materialize';
+
+
+export default class StudentReportForGrading extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>Student Report For Grading</div>
+        );
+    }
+}
