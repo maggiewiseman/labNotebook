6ac2e676f271f9c4d267a6273581ff8863b0119a
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-27 11:32:10 +0200

    wrote checks for student and teacher

--- routers/middleware.js
+++ routers/middleware.js
@@ -16,7 +16,7 @@ function loggedInCheck(req, res, next) {
     if(req.session.user) {
         //logged in!
         console.log('user is logged in sending to next');
-        
+
         next();
     } else {
         res.redirect('/');
@@ -24,6 +24,23 @@ function loggedInCheck(req, res, next) {
 }
 
 //add student check and teacher check.
+function checkIfTeacher(req, res, next) {
+    if(req.session.user.role == 'teacher'){
+        next();
+    } else {
+        res.redirect('/student');
+    }
+}
 
+function checkIfStudent(req, res, next) {
+    if(req.session.user.role == 'student'){
+        next();
+    } else {
+        res.redirect('/teacher');
+    }
+}
+
+module.exports.checkIfTeacher = checkIfTeacher;
+module.exports.checkIfStudent = checkIfStudent;
 module.exports.loggedInCheck = loggedInCheck;
 module.exports.registerLoginCheck = registerLoginCheck;
