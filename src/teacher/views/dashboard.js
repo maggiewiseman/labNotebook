c80c6755b668516bfdb18940262b41c3b6f0407c
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-28 22:42:06 +0200

    added hellow need to add user name

--- src/teacher/views/dashboard.js
+++ src/teacher/views/dashboard.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {MediaBox, Navbar, NavItem, Row, Col, Container, SideNav, SideNavItem, Button, Collapsible, CollapsibleItem, Modal, Input, Collection, CollectionItem, Card} from 'react-materialize';
 import { connect } from 'react-redux';
 
-export default function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
+class HelloWorld extends React.Component {
+
+    render() {
+        return (
+            <Card title={`Hello`}></Card>
+        );
+    }
 }
+
+var mapStateToProps = function(state) {
+    return {
+        currUser: state.teachers.currUser
+    }
+}
+
+export default connect(mapStateToProps)(HelloWorld);
