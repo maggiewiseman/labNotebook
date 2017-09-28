41ead2cb084a1f9d1841858be151a62f3b949a59
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-09-28 10:58:26 +0200

    got categories showing up

--- src/teacher/views/gradeCategory.js
+++ src/teacher/views/gradeCategory.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import axios  from '../../api/axios';
 import { getCategoriesForGrading, getStudentAssignmentList } from '../actions';
 
 import {Row, Col, Button, Input, Card, Collection, CollectionItem } from 'react-materialize';
@@ -12,28 +11,41 @@ class GradeACategory extends React.Component {
         this.state = {
             titles: []
         }
+        this.handleChange = this.handleChange.bind(this);
     }
-    componentWillMount() {
+    handleChange(e) {
+
+    }
+    componentDidMount() {
         if(!this.props.studentAssignmentList) {
             this.props.dispatch(getStudentAssignmentList(this.props.params.sectionid));
         }
-        console.log("GradeACategory", this.props.params);
+        console.log("GradeACategory", this.props);
         const { assignmentid, category} = this.props.params;
         this.props.dispatch(getCategoriesForGrading(assignmentid, category))
-    }//end component will mount
+    }//end component did mount
     render() {
-        const { studentCategoryData } = this.props;
-        const { sectionid, assignmentid } = this.props.params;
-        var gradeList = makeList(studentCategoryData, sectionid, assignmentid);
-        return (
-            <div>
-                Category Grading...
-            </div>
-        )
+        console.log('PROPS in gradeACategory:', this.props);
+        if(!this.props.currAssignmentId){
+            console.log('returning null');
+            return null;
+        }
+            const { studentCategoryData } = this.props;
+            const { sectionid, assignmentid } = this.props.params;
+            var gradeList = makeList(studentCategoryData, sectionid, assignmentid, this.handleChange);
+            console.log("GRADELIST: ", gradeList);
+            return (
+                <div>
+                    <p>Grade these:</p>
+                    {gradeList}
+                </div>
+            )
+
     }
 }
 
 /************ CONNECTED COMPONENT *************/
+
 var mapStateToProps = function(state) {
     return {
         studentCategoryData: state.teachers.studentCategoryData,
@@ -43,11 +55,24 @@ var mapStateToProps = function(state) {
 }
 export default connect(mapStateToProps)(GradeACategory);
 
+
+
 /*************** HELPER FUNCTIONS ***************/
 function makeList(data) {
-
-    // var cards = data.map(student => {
-    //
-    // })
-    return 'list';
+    console.log('Make list', data, event);
+    return data.map(studentData => {
+        return (
+            <Row>
+                <Col s={12} m={6}>
+                    <p>Name: </p>
+                    <div>
+                    {studentData.content}
+                    </div>
+                </Col>
+                <Col s={12} m={6}>
+                    <Input type="textarea" name={`comments_${studentData.id}`} onChange={event} label="Comments"/>
+                    <Input type="text" label="Grade" onChange={event}/>
+                </Col>
+            </Row>);
+    });
 }
